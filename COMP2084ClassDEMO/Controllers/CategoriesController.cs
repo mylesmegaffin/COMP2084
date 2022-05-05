7da22b7bdecbbfb9811f8d70a8f2f1cd79547ccd
mylesmegaffin <mylesmegaffin@gmail.com>
2022-05-05 17:31:28 -0400

    Add project files.

---
+++ COMP2084ClassDEMO/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using COMP2084ClassDEMO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084ClassDEMO.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            //use the category model to create 10 fake categories and send to the view for display
+            //create an empty list of categories
+            var categories = new List<Category>();
+
+            //use a loop to create 10 fake categories
+            for (int i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass the final list to the view for display
+            return View(categories);
+        }
+
+        public IActionResult Browse(string categoryName)
+        {
+            //take the category name passed in with the link and store it in the viewbag for display
+            ViewBag.categoryName = categoryName;
+            // load the view /Views/Categories/Browse
+            return View();
+        }
+    }
+}
---
+++ COMP2084ClassDEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP2084ClassDEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
