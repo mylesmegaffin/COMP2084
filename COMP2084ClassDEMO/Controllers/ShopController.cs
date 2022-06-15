8484151141e08ffdc6ad3f84cc8e71e0dd953524
mylesmegaffin <mylesmegaffin@gmail.com>
2022-06-15 18:56:13 -0400

    Customer Shop View

---
+++ COMP2084ClassDEMO/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using COMP2084ClassDEMO.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084ClassDEMO.Controllers
+{
+    public class ShopController : Controller
+    {
+        // db connection
+        private readonly ApplicationDbContext _context;
+
+        // connect to the db whenever this controller is used 
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //get the list of categories to display to customers on the main shopping page 
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
